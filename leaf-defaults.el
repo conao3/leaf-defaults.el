cec7535b50227ddaa6fdeed692e46039677623e8
Naoya Yamashita <conao3@gmail.com>
2021-02-26 03:44:39 +0900

    add normalizers, handlers

--- leaf-defaults.el
+++ leaf-defaults.el
@@ -28,6 +28,7 @@
 
 ;;; Code:
 
+(require 'cl-lib)
 (require 'leaf)
 (require 'leaf-keywords)
 
@@ -35,6 +36,61 @@
   "Awesome leaf config collections."
   :group 'convenience
   :link '(url-link :tag "Github" "https://github.com/conao3/leaf-defaults.el"))
+
+(defcustom leaf-defaults-before-protection
+  (leaf-list
+   :convert-defaults `((defun ,(car leaf--value) () ,(cdr leaf--value) ,@leaf--body)))
+  "Additional `leaf-keywords' before protection.
+:disabled <this place> :leaf-protect"
+  :set #'leaf-keywords-set-keywords
+  :type 'sexp
+  :group 'leaf-defaults)
+
+(defcustom leaf-defaults-after-conditions
+  (leaf-list
+   :defaults   `(,@(mapcar (lambda (elm) `(,elm)) leaf--value) ,@leaf--body))
+  "Additional `leaf-keywords' after conditional branching.
+:when :unless :if ... :ensure <this place> :after"
+  :set #'leaf-keywords-set-keywords
+  :type 'sexp
+  :group 'leaf-defaults)
+
+(defcustom leaf-defaults-normalize
+  '(((memq leaf--key '(:defaults))
+     (let ((ret (leaf-flatten leaf--value)))
+       (if (eq nil (car ret))
+           nil
+         (mapcar
+          (lambda (elm)
+            (intern (format "leaf-keywords-defaults--%s/%s" (if (eq t elm) "leaf" elm) leaf--name)))
+          (delete-dups ret)))))
+
+    ((memq leaf--key '(:convert-defaults))
+     (let* ((key (car leaf--value))
+            (sym (intern (format "%s/%s" (if (eq t key) "leaf" key) leaf--name))))
+       `(,(intern (format "leaf-keywords-defaults--%s" sym))
+         . ,(format "Default config for %s." sym)))))
+  "Additional `leaf-normalize'."
+  :set #'leaf-keywords-set-normalize
+  :type 'sexp
+  :group 'leaf-defaults)
+
+(defun leaf-defaults-init (&optional force)
+  "Initialize leaf-defaults, add :defaults, :convert-defaults for `leaf.'.
+If FORCE is non-nil, append our element without any conditions."
+  (when (or force (not (memq :defaults leaf-keywords-after-conditions)))
+    (setq leaf-keywords-after-conditions
+          (append leaf-defaults-before-protection leaf-keywords-after-conditions)))
+
+  (when (or force (not (memq :defaults leaf-keywords-after-conditions)))
+    (setq leaf-keywords-after-conditions
+          (append leaf-defaults-before-protection leaf-keywords-after-conditions)))
+
+  (when (or force (not (cl-member-if
+                        (lambda (elm) (equal (car elm) '(memq leaf--key '(:defaults))))
+                        leaf-keywords-normalize)))
+    (setq leaf-defaults-normalize
+          (append leaf-defaults-normalize leaf-keywords-normalize))))
 
 (provide 'leaf-defaults)
 
