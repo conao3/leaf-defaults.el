4892abb87cb84c3a5914c542124f66ef22c9d5ce
Naoya Yamashita <conao3@gmail.com>
2021-02-26 01:59:25 +0900

    generate project

---
+++ leaf-defaults-tests.el
@@ -0,0 +1,34 @@
+;;; leaf-defaults-tests.el --- Test definitions for leaf-defaults  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; URL: https://github.com/conao3/leaf-defaults.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Test definitions for `leaf-defaults'.
+
+
+;;; Code:
+
+(require 'cort)
+(require 'leaf-defaults)
+
+
+;; (provide 'leaf-defaults-tests)
+
+;;; leaf-defaults-tests.el ends here
